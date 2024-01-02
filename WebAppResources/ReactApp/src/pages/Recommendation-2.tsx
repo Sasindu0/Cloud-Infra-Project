bafa56c5efbe3ded5cb89d38737288cc25bd7d2c
Sasindu0 <larryking3568@gmail.com>
2024-01-03 01:45:48 +0530

    dist Updated

--- WebAppResources/ReactApp/src/pages/Recommendation-2.tsx
+++ WebAppResources/ReactApp/src/pages/Recommendation-2.tsx
@@ -11,7 +11,7 @@ import {
 import "./Recommendation-2.css";
 import "./general.css";
 
-//asdftysdfdsfsdffddsfdsdfrtt
+//asdftysdfdsfsdffddsfdsdfrttdfgdfgdg
 
 const baseUrl: string = "https://d236qntmmi6g9k.cloudfront.net";
 
